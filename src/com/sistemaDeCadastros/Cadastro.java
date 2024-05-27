9849f67debf74694f30620ae9e9df7f762e78c9b
AlexReisC <alex.reis@aluno.ufca.edu.br>
2024-05-27 18:41:22 -0300

    feat: method to register new user
    register user and create a file '.txt' for the user with your answers

--- src/com/sistemaDeCadastros/Cadastro.java
+++ src/com/sistemaDeCadastros/Cadastro.java
@@ -2,20 +2,18 @@ package com.sistemaDeCadastros;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
-import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 
-class listaArquivos extends SimpleFileVisitor<Path>{
+class ListaArquivos extends SimpleFileVisitor<Path>{
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
@@ -51,13 +49,16 @@ public class Cadastro {
         }
     }
 
-    public static void criarArquivo(Usuario usuario) {
-        String path = "C:\\Estudos e Projetos\\Projetos\\Sistema de Cadastros\\src\\com\\sistemaDeCadastros\\";
-        String nomeDoArquivo = usuario.getNome().toUpperCase().replace(" ", "");
-        nomeDoArquivo = usuariosCadastrados.size() + "-" + nomeDoArquivo + ".txt";
-        path = path + nomeDoArquivo;
-
-        Path arquivoPath = Paths.get(path);
+    public static void criarArquivo(List<String> respotas) {
+        Path path = Paths.get("C:\\Estudos e Projetos\\Projetos\\Sistema de Cadastros\\src\\com\\sistemaDeCadastros\\");
+        try {
+            Files.walkFileTree(path, new ListaArquivos());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String nomeDoArquivo = (usuariosCadastrados.size()+1) + "-" + respotas.getFirst().toUpperCase().replace(" ", "") + ".txt";
+        String pathString = path.toString() + "\\" + nomeDoArquivo;
+        Path arquivoPath = Paths.get(pathString);
 
         try {
             Files.createFile(arquivoPath);
@@ -65,55 +66,36 @@ public class Cadastro {
             System.out.println("O arquivo deste usuário já existe");
         }
 
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
-            bw.write(usuario.getNome());
-            bw.newLine();
-            bw.write(usuario.getEmail());
-            bw.newLine();
-            bw.write(String.format("%d", usuario.getIdade()));
-            bw.newLine();
-            bw.write(String.format("%.2f", usuario.getAltura()));
-            bw.flush();
+        try (BufferedWriter writer = Files.newBufferedWriter(arquivoPath)) {
+            for (int i = 0; i < respotas.size(); i++) {
+                writer.write(respotas.get(i));
+                writer.newLine();
+            }
+            writer.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public static void cadastrarUsuario() {
-        Usuario usuario = new Usuario();
+        List<String> respostas = new ArrayList<>();
+
         Path formularioPath = Paths.get(
                 "C:\\Estudos e Projetos\\Projetos\\Sistema de Cadastros\\src\\com\\sistemaDeCadastros\\formulario.txt");
         try (Scanner scan = new Scanner(System.in)) {
             try (BufferedReader bufferedReader = Files.newBufferedReader(formularioPath)) {
                 String linha;
                 while ((linha = bufferedReader.readLine()) != null) {
-                    perguntas.add(linha);
+                    System.out.println(linha);
+                    String texto = scan.nextLine();
+                    respostas.add(texto);
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
-            System.out.println(perguntas.get(0));
-            usuario.setNome(scan.nextLine());
-            
-            System.out.println(perguntas.get(1));
-            usuario.setEmail(scan.nextLine());
-            
-            System.out.println(perguntas.get(2));
-            usuario.setIdade(scan.nextInt());
-            
-            System.out.println(perguntas.get(3));
-            try {
-                usuario.setAltura(scan.nextDouble());
-            } catch (InputMismatchException e) {
-                System.out.println("Use virgula ao invés de ponto:");
-                scan.nextLine();
-                usuario.setAltura(scan.nextDouble());
-            }
         }
-
-        quantidadeUsuarios++;
-        System.out.println("\n" + usuario.toString());
+        
+        criarArquivo(respostas);
     }
 
     public static void listarUsuarios(){
